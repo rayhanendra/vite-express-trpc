078195b924362040a953a68ecf5ef1ea4a065589
Rayhanendra <mrayhann@gmail.com>
2023-05-18 23:11:55 +0700

    implement user

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,13 +2,18 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import { AppRouter } from '../../server/api';
 
 const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: 'http://localhost:3000/trpc' })],
+  links: [httpBatchLink({ url: 'http://localhost:5173/trpc' })],
 });
 
 async function main() {
   // const result = await client.sayHi.query();
 
-  const result = await client.logToServer.mutate('hello from client');
+  // const result = await client.logToServer.mutate('hello from client');
+  // const result = await client.users.get.query({ userId: '123' });
+  const result = await client.users.update.mutate({
+    userId: '123',
+    name: 'John Doe',
+  });
   console.log(result);
 }
 
