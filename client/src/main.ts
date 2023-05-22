2b614e29bf06fa14780390475c88078ccb4a8540
nendra <mrayhann@gmail.com>
2023-05-22 09:06:51 +0700

    feat: implement admin, add loggerLink, fix url

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,19 +1,28 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client';
 import { AppRouter } from '../../server/api';
 
 const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: 'http://localhost:5173/trpc' })],
+  links: [
+    loggerLink(),
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+      headers: {
+        Authorization: 'Bearer 123',
+      },
+    }),
+  ],
 });
 
 async function main() {
   // const result = await client.sayHi.query();
-
   // const result = await client.logToServer.mutate('hello from client');
   // const result = await client.users.get.query({ userId: '123' });
-  const result = await client.users.update.mutate({
-    userId: '123',
-    name: 'John Doe',
-  });
+  // const result = await client.users.update.mutate({
+  //   userId: '123',
+  //   name: 'John Doe',
+  // });
+  // console.log(result);
+  const result = await client.secretData.query();
   console.log(result);
 }
 
