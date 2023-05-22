802825b68179153195460746128e7b7a34194926
nendra <mrayhann@gmail.com>
2023-05-22 16:42:37 +0700

    feat(client): implement websocket

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,16 +1,42 @@
-import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client';
+import {
+  createTRPCProxyClient,
+  createWSClient,
+  httpBatchLink,
+  loggerLink,
+  splitLink,
+  wsLink,
+} from '@trpc/client';
 import { AppRouter } from '../../server/api';
+
+const wsClient = createWSClient({
+  url: 'ws://localhost:3000/trpc',
+});
 
 const client = createTRPCProxyClient<AppRouter>({
   links: [
     loggerLink(),
-    httpBatchLink({
-      url: 'http://localhost:3000/trpc',
-      headers: {
-        Authorization: 'Bearer 123',
+    splitLink({
+      condition: (op) => {
+        return op.type === 'subscription';
       },
+      true: wsLink({
+        client: wsClient,
+      }),
+      false: httpBatchLink({
+        url: 'http://localhost:3000/trpc',
+        headers: {
+          Authorization: 'Bearer 123',
+        },
+      }),
     }),
   ],
+});
+
+document.addEventListener('click', () => {
+  client.users.update.mutate({
+    userId: '123',
+    name: 'John Doe',
+  });
 });
 
 async function main() {
@@ -22,8 +48,14 @@ async function main() {
   //   name: 'John Doe',
   // });
   // console.log(result);
-  const result = await client.secretData.query();
-  console.log(result);
+  // const result = await client.secretData.query();
+  // console.log(result);
+  const connection = client.users.onUpdate.subscribe(undefined, {
+    onData: (id) => {
+      console.log('user updated', id);
+    },
+  });
+  wsClient.close();
 }
 
 main();
